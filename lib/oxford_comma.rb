9bb5edcf078f96551dc425d6f12509bacff6d816
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-07 13:51:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,27 @@
 
+# def oxford_comma(array)
+#   if array.length == 1
+#     return "#{array[0]}"
+#   elsif array.length == 2
+#     return array.join(" and ")
+#   elsif array.length >= 3
+#     new_last_array_item = "and #{array[-1]}"
+#     array.pop
+#     array.push(new_last_array_item)
+#     return array.join(", ")
+#   end
+# end
+
 def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
-    new_last_array_item = "and #{array[-1]}"
-    array.pop
-    array.push(new_last_array_item)
+    array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
 end
-
-
 
 
 # takes an argument array of string elements
