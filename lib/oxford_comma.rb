6cb415e6dbf6b7a897ee62fccf11d44c778e380f
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-07 13:25:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,13 @@
-# def oxford_comma(array)
-#   if array.size == 1 
-#     return array[0]
-#   elsif array.size == 2
-#     return array.join(" and ")
-#   else 
-#     return array[0..-2].join(',') + ", and " + array[-1]
-#   end
-# end
-
 def oxford_comma(array)
-  array.size <= 2? array.join(' and '):(array.first array.size - 1).join(',') + ", and #{array.last}"
+  if array.size == 1 
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else 
+    return array[0..-2].join(',') + ", and " + array[-1]
+  end
 end
+
 
 # takes an argument array of string elements
 # converts it into a string using the Oxform comma
