cb0cd6009cba33861949ee298be8243e570f51c6
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-07 13:19:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,8 @@ def oxford_comma(array)
 end
 
 def oxford_comma(array)
-  array.size <= 2? a.join(' and '):(a.first a.size - 1).join(',')
+  array.size <= 2? a.join(' and '):(a.first a.size - 1).join(',') + ", and #{a.last}"
+end
 
 # takes an argument array of string elements
 # converts it into a string using the Oxform comma
