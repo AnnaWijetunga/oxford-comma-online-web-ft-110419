b9eb1de382575eef1c8bac6968423b60fa67ae51
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-07 13:22:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,16 @@
-def oxford_comma(array)
-  if array.size == 1 
-    return array[0]
-  elsif array.size == 2
-    return array.join(" and ")
-  else 
-    return array[0..-2].join(',') + ", and " + array[-1]
-  end
-end
-
 # def oxford_comma(array)
-#   array.size <= 2? a.join(' and '):(a.first a.size - 1).join(',') + ", and #{a.last}"
+#   if array.size == 1 
+#     return array[0]
+#   elsif array.size == 2
+#     return array.join(" and ")
+#   else 
+#     return array[0..-2].join(',') + ", and " + array[-1]
+#   end
 # end
+
+def oxford_comma(array)
+  array.size <= 2? array.join(' and '):(array.first array.size - 1).join(',') + ", and #{array.last}"
+end
 
 # takes an argument array of string elements
 # converts it into a string using the Oxform comma
