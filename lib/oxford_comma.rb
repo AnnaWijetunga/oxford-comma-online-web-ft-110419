55a091bee67020faea26165fcdcaaeaddfa90136
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-07 13:19:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
-def oxford_comma(array)
-  if array.size == 1 
-    return array[0]
-  elsif array.size == 2
-    return array.join(" and ")
-  else 
-    return array[0..-2].join(',') + ", and " + array[-1]
-  end
-end
+# def oxford_comma(array)
+#   if array.size == 1 
+#     return array[0]
+#   elsif array.size == 2
+#     return array.join(" and ")
+#   else 
+#     return array[0..-2].join(',') + ", and " + array[-1]
+#   end
+# end
 
 def oxford_comma(array)
   array.size <= 2? a.join(' and '):(a.first a.size - 1).join(',') + ", and #{a.last}"
